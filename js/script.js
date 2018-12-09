194169f5a124a73cc0684be1a2718b46ab0615ef
Damian McNulty <damianmcnulty@me.com>
2018-12-09 15:20:33 +0000

    total expenses

--- js/script.js
+++ js/script.js
@@ -66,6 +66,22 @@ d3.csv("data/Accounts.csv", function (data) {
     var all = ndx.groupAll();
     var ndGroup = all.reduceSum(function (d) { return d.amount });
 
+       //Source: https://stackoverflow.com/questions/34744243/d3-js-format-as-currency-euro
+    var IE = d3.locale({
+        "decimal": ".",
+        "thousands": ",",
+        "grouping": [3],
+        "currency": ["€", ""],
+        "dateTime": "%a %b %e %X %Y",
+        "date": "%m/%d/%Y",
+        "time": "%H:%M:%S",
+        "periods": ["AM", "PM"],
+        "days": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+        "shortDays": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+        "months": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        "shortMonths": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    })
+    
     var cur = IE.numberFormat("$,.2f");
     
     dc.numberDisplay("#total-amount-display")
