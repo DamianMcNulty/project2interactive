a2813c423943a0361e70bbed9a59437ab4a94b9e
Damian McNulty <damianmcnulty@me.com>
2019-06-29 22:13:00 +0100

    added groceries

--- runserver.py
+++ runserver.py
@@ -20,7 +20,7 @@ mongo = PyMongo(app)
 
 @app.route('/data')
 def data():
-    return dumps(list(mongo.db.goceries.find()))
+    return dumps(list(mongo.db.groceries.find()))
 
 @app.route('/')
 def index():
